88d9596ac65dd37231079be877c9b779c0985958
Suraj <surajdhote44@gmail.com>
2025-03-08 02:35:52 +0530

    db connect

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongodb+srv://root:<db_password>@cluster0.3fbun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/application_system', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:<Suraj7972@>@cluster0.xo3ff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
